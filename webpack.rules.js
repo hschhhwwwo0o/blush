05b5f15f4bc5e39a8d60eb065b2c1f78ba8b918a
Saveliy <seva007@hotmail.com>
2021-04-04 12:43:57 +0300

    Correct rules

--- webpack.rules.js
+++ webpack.rules.js
@@ -2,25 +2,25 @@ module.exports = [
   // Add support for native node modules
   {
     test: /\.node$/,
-    use: 'node-loader',
+    use: "node-loader",
   },
   {
     test: /\.(m?js|node)$/,
     parser: { amd: false },
     use: {
-      loader: '@marshallofsound/webpack-asset-relocator-loader',
+      loader: "@marshallofsound/webpack-asset-relocator-loader",
       options: {
-        outputAssetBase: 'native_modules',
+        outputAssetBase: "native_modules",
       },
     },
   },
   {
     test: /\.jsx?$/,
     use: {
-      loader: 'babel-loader',
+      loader: "babel-loader",
       options: {
         exclude: /node_modules/,
-        presets: ['@babel/preset-react']
+        presets: ["@babel/preset-react"]
       }
     }
   },
