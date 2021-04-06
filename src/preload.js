826009fdd81a48071046974c6682cc79fa4d01a7
Saveliy <seva007@hotmail.com>
2021-04-06 11:05:43 +0300

    Initial preload
    Add tracks

--- src/preload.js
+++ src/preload.js
@@ -1,1 +1,37 @@
-console.log("preload")
+const fs = require("fs");
+
+const __HOME_DIR = require("os").userInfo().homedir;
+
+window.STORE = {};
+
+const PATHS_FILES = [
+    `${__HOME_DIR}\\Music\\test`,
+]
+
+const GET_AUDIO = (PATHS_DIRS) => {
+
+    const __GET_PATHS = () => {
+        try {
+            let fls = new Array();
+    
+            PATHS_DIRS.forEach( (path) => {
+                const files = fs.readdirSync(path)
+                const paths = files.map( (file) => {
+                    return `${path}\\${file}`
+                } )
+    
+                fls = [...fls, paths]
+            } )
+    
+            return fls
+    
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    STORE.TRACKS = __GET_PATHS()[0]
+
+}
+
+GET_AUDIO(PATHS_FILES);
