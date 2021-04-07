286838d00eefa54d9d77974e979625f708df8e6f
Saveliy <seva007@hotmail.com>
2021-04-07 13:18:13 +0300

    Redesign concept
    Remove preload.js

--- src/init.jsx
+++ src/init.jsx
@@ -1,11 +1,77 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-function render() {
-    ReactDOM.render(
-        <h2>Hello from React!</h2>, 
-        document.querySelector("#root")
-    );
+import OS from "os";
+import fs from "fs";
+import { parseFile } from "music-metadata";
+
+const __HOME_DIR = OS.userInfo().homedir;
+
+const PATHS_DIRS = [
+    `${__HOME_DIR}\\Music\\test`,
+]
+
+console.log("start")
+
+const __GET_PATHS = () => {
+    try {
+        let fls = new Array();
+
+        PATHS_DIRS.forEach( (path) => {
+            const files = fs.readdirSync(path)
+            const paths = files.map( (file) => {
+                return `${path}\\${file}`
+            } )
+
+            fls = [...fls, paths]
+        } )
+
+        return fls[0]
+
+    } catch (error) {
+        console.error(error)
+    }
 }
 
-render();
+const getMetaTags = async () => {
+
+
+    let data = __GET_PATHS().map( async (URI) => {
+
+        const metadata = await parseFile(URI);
+
+        const buffer = fs.readFileSync(URI);
+        const blob = new Blob([buffer], { type: "audio/mp3" });
+        const url = window.URL.createObjectURL(blob);
+
+        metadata.url = url;
+
+        return {
+            url:        url,
+            artist:     metadata.common.artist,
+            title:      metadata.common.title
+        }
+
+    } )
+
+    Promise.all(data)
+    
+    .then( (data) => { 
+
+        console.log(data)
+
+        function render() {
+            console.log(data)
+            ReactDOM.render( <h2>Blush</h2>, document.querySelector("#root"));
+        }
+
+        render();
+
+    } )
+
+}
+
+
+getMetaTags()
+
+console.log(__GET_PATHS())
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 const { app, BrowserWindow } = require("electron");
-const path = require("path");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) { // eslint-disable-line global-require
@@ -12,7 +11,6 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
       contextIsolation: false,
       nodeIntegration: true
     }
