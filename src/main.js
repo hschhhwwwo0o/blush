dd69c150613e35676d1d9f3de9adb433f4c9bfd6
Saveliy <seva007@hotmail.com>
2021-04-08 16:36:44 +0300

    Settings main.js

--- src/main.js
+++ src/main.js
@@ -8,8 +8,9 @@ if (require("electron-squirrel-startup")) { // eslint-disable-line global-requir
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 807,
+    height: 879,
+    resizable: false,
     webPreferences: {
       contextIsolation: false,
       nodeIntegration: true
@@ -20,7 +21,7 @@ const createWindow = () => {
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
