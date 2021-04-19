e5ddcb17cdd32bb5d1b083dd2fbda2beca047724
Saveliy <seva007@hotmail.com>
2021-04-19 17:01:50 +0300

    SNTHX

--- src/utils/__GET_PATHS.js
+++ src/utils/__GET_PATHS.js
@@ -1,22 +1,24 @@
-import fs from "fs";
+import { readdirSync } from "fs";
 
 const __GET_PATHS = (PATHS_DIRS) => {
     try {
         let fls = new Array();
 
         PATHS_DIRS.forEach( (path) => {
-            const files = fs.readdirSync(path)
+
+            const files = readdirSync(path);
+
             const paths = files.map( (file) => {
                 return `${path}\\${file}`
             } )
 
-            fls = [...fls, paths]
+            fls = [...fls, paths];
         } )
 
-        return fls[0]
+        return fls[0];
 
     } catch (error) {
-        console.error(error)
+        console.error(error);
     }
 }
 
