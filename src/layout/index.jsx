fb08aa53906d48e733fb41e7b389d01990921daf
Saveliy <seva007@hotmail.com>
2021-04-22 18:37:15 +0300

    Titlebar

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 
 const Index = ({ data }) => {
+
+    const closeWindow = () => {
+        window.top.close()
+    }
+
     return <>
         <main id="app">
-            <div id="titlebar"></div>
+            <div id="titlebar">
+                <div></div>
+                <div id="titlebar__tools">
+                    <div></div>
+                    <div id="titlebar__close-button" onClick={ closeWindow }>
+                        <div />
+                    </div>
+                </div>
+            </div>
         </main>
     </>
 }
