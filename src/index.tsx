b43fdff07ad3b4330aff9f781485683d0de35cdf
Saveliy <seva007@hotmail.com>
2021-05-04 07:32:42 +0300

    Correct entry point

--- src/index.tsx
+++ src/index.tsx
@@ -2,27 +2,15 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import __GET_DATA from "./utils/__GET_DATA";
-
 import MainLayout from "./layout/index";
 
-const __RENDER = async () => {
-
-    __GET_DATA()
-    
-    .then( (data) => {
-
-        function render() {
-            console.log(data)
-            ReactDOM.render( 
-                <MainLayout data={ data } />, 
-                document.querySelector("#root")
-            );
-        }
-
-        render();
-
-    } )
-
+async function __R() {
+    __GET_DATA().then((data) => {
+        ReactDOM.render( 
+            <MainLayout data={ data } />, 
+            document.querySelector("#root") 
+        );
+    });
 }
 
-__RENDER();
+__R();
