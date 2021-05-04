c4fff58a6a13dd305259f9355a5ae70ccc58e28f
Saveliy <seva007@hotmail.com>
2021-05-04 09:23:40 +0300

    Load font

--- src/components/Track/index.tsx
+++ src/components/Track/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Track: React.FunctionComponent = () => {
     return <>
-        
+    
     </>
 }
 
--- webpack.rules.js
+++ webpack.rules.js
@@ -1,39 +1,38 @@
 module.exports = [
-  // Add support for native node modules
-  {
-    test: /\.node$/,
-    use: "node-loader",
-  },
-  {
-    test: /\.(m?js|node)$/,
-    parser: { amd: false },
-    use: {
-      loader: "@marshallofsound/webpack-asset-relocator-loader",
-      options: {
-        outputAssetBase: "native_modules",
-      },
+    {
+        test: /\.node$/,
+        use: "node-loader",
     },
-  },
-  {
-    test: /\.tsx?$/,
-    exclude: /(node_modules|\.webpack)/,
-    use: {
-      loader: "ts-loader",
-      options: {
-        transpileOnly: true
-      }
-    }
-  },
-  {
-    test: /\.(png|jpe?g|gif|svg)$/i,
-    use: [
-      {
-        loader: "file-loader",
+    {
+        test: /\.(m?js|node)$/,
+        parser: { amd: false },
+        use: {
+        loader: "@marshallofsound/webpack-asset-relocator-loader",
         options: {
-          publicPath: "../.", // move up from 'main_window'
-          context: "src", // set relative working folder to src
+            outputAssetBase: "native_modules",
+        },
+        },
+    },
+    {
+        test: /\.tsx?$/,
+        exclude: /(node_modules|\.webpack)/,
+        use: {
+        loader: "ts-loader",
+        options: {
+            transpileOnly: true
         }
-      },
-    ],
-  },
+        }
+    },
+    {
+        test: /\.(png|jpe?g|gif|svg|eot|svg|ttf|woff|woff2)$/i,
+        use: [
+            {
+                loader: "file-loader",
+                options: {
+                    publicPath: "../.", // move up from 'main_window'
+                    context: "src", // set relative working folder to src
+                }
+            },
+        ],
+    }
 ];
