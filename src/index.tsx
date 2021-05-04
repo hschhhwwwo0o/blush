36d485c2ec33b2a24b747534ef6b5b37b1f167f3
Saveliy <seva007@hotmail.com>
2021-05-04 08:47:22 +0300

    Use removeUndefined

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,12 @@ import * as ReactDOM from "react-dom";
 
 import createDataFromFS from "./utils/createDataFromFS";
 import MainLayout from "./layout/index";
+import removeUndefined from "./utils/removeUndefined";
 
 async function __R() {
     createDataFromFS().then((data) => {
         ReactDOM.render( 
-            <MainLayout data={ data } />, 
+            <MainLayout data={ removeUndefined(data) } />, 
             document.querySelector("#root") 
         );
     });
