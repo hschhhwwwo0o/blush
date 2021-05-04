b36a56fe014d0f1dfe2a388fa9a4da623b665086
Saveliy <seva007@hotmail.com>
2021-05-04 09:01:49 +0300

    Initial structure app

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,16 +1,21 @@
 import React from "react";
+
 import TheTitlebar from "../components/TheTitlebar";
+import ThePlayer from "../components/ThePlayer";
+import Track from "../components/Track";
 
 import { ITrack } from "../types";
 
-interface IData {
-    data: ITrack[]
-}
-
-const Index: React.FunctionComponent<IData> = ({ data }) => {
+const Index: React.FunctionComponent<{ data: ITrack[] }> = ({ data }) => {
     return <>
         <main id="app">
             <TheTitlebar />
+            <ThePlayer />
+            {
+                data.map( (track, id) => {
+                    return <Track key={id} />
+                } )
+            }
         </main>
     </>
 }
