84ffa7362b38f095f4722c44eeb9436fec75884c
Saveliy <seva007@hotmail.com>
2021-05-13 19:14:18 +0300

    Structure redux

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,4 +1,5 @@
 import { createStore } from "redux";
+import reducer from "./reducer";
 
 const intialStatePlayer = {
     isPlay: false,
@@ -7,77 +8,7 @@ const intialStatePlayer = {
     currentTimeLine: 0,
     currentTime: 0,
 }
-
-function PlayerReducer(state = intialStatePlayer, action) {
-    switch (action.type) {
-        case "PLAY":
-            return {
-                isPlay: true,
-                nowPlay: state.nowPlay,
-                isAutoPlay: true,
-                currentTimeLine: state.currentTimeLine,
-                currentTime: state.currentTime,
-            }
-
-        case "PAUSE":
-            return {
-                isPlay: false,
-                nowPlay: state.nowPlay,
-                isAutoPlay: true,
-                currentTimeLine: state.currentTimeLine,
-                currentTime: state.currentTime,
-            }
-
-        case "NEXT":
-            return {
-                isPlay: true,
-                nowPlay: state.nowPlay + 1,
-                isAutoPlay: true,
-                currentTimeLine: 0,
-                currentTime: 0,
-            }
-
-        case "PREV":
-            return {
-                isPlay: true,
-                nowPlay: state.nowPlay - 1,
-                isAutoPlay: true,
-                currentTimeLine: 0,
-                currentTime: 0,
-            }
-
-        case "TIMELINE_PLAY":
-            return {
-                isPlay: state.isPlay,
-                nowPlay: state.nowPlay,
-                isAutoPlay: state.isAutoPlay,
-                currentTimeLine: state.currentTimeLine + 1,
-                currentTime: state.currentTime + 1,
-            }
-
-        case "TIMELINE_PAUSE":
-            return {
-                isPlay: state.isPlay,
-                nowPlay: state.nowPlay,
-                isAutoPlay: state.isAutoPlay,
-                currentTimeLine: state.currentTimeLine,
-                currentTime: state.currentTime,
-            }
-
-        case "CHANGE_TIMELINE":
-            return {
-                isPlay: state.isPlay,
-                nowPlay: state.nowPlay,
-                isAutoPlay: state.isAutoPlay,
-                currentTimeLine: action.currentTimeLine,
-                currentTime: action.currentTimeLine,
-            }
-
-        default:
-            return state
-    }
-}
   
-const store = createStore(PlayerReducer, intialStatePlayer);
+const store = createStore(reducer, intialStatePlayer);
 
 export default store;
---
+++ src/redux/reducer/index.js
@@ -0,0 +1,71 @@
+function PlayerReducer(state = intialStatePlayer, action) {
+    switch (action.type) {
+        case "PLAY":
+            return {
+                isPlay: true,
+                nowPlay: state.nowPlay,
+                isAutoPlay: true,
+                currentTimeLine: state.currentTimeLine,
+                currentTime: state.currentTime,
+            }
+
+        case "PAUSE":
+            return {
+                isPlay: false,
+                nowPlay: state.nowPlay,
+                isAutoPlay: true,
+                currentTimeLine: state.currentTimeLine,
+                currentTime: state.currentTime,
+            }
+
+        case "NEXT":
+            return {
+                isPlay: true,
+                nowPlay: state.nowPlay + 1,
+                isAutoPlay: true,
+                currentTimeLine: 0,
+                currentTime: 0,
+            }
+
+        case "PREV":
+            return {
+                isPlay: true,
+                nowPlay: state.nowPlay - 1,
+                isAutoPlay: true,
+                currentTimeLine: 0,
+                currentTime: 0,
+            }
+
+        case "TIMELINE_PLAY":
+            return {
+                isPlay: state.isPlay,
+                nowPlay: state.nowPlay,
+                isAutoPlay: state.isAutoPlay,
+                currentTimeLine: state.currentTimeLine + 1,
+                currentTime: state.currentTime + 1,
+            }
+
+        case "TIMELINE_PAUSE":
+            return {
+                isPlay: state.isPlay,
+                nowPlay: state.nowPlay,
+                isAutoPlay: state.isAutoPlay,
+                currentTimeLine: state.currentTimeLine,
+                currentTime: state.currentTime,
+            }
+
+        case "CHANGE_TIMELINE":
+            return {
+                isPlay: state.isPlay,
+                nowPlay: state.nowPlay,
+                isAutoPlay: state.isAutoPlay,
+                currentTimeLine: action.currentTimeLine,
+                currentTime: action.currentTimeLine,
+            }
+
+        default:
+            return state
+    }
+}
+
+export default PlayerReducer;
