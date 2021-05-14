3f9be0cc9fda7df97e59128cedc3eb1468dff923
Saveliy <seva007@hotmail.com>
2021-05-14 14:47:36 +0300

    CS

--- src/index.app.tsx
+++ src/index.app.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
+import { useSelector } from "react-redux";
+
 import Layout from "./layout/index";
 
 import { ITrack } from "./types";
-
-import { useSelector, useDispatch } from 'react-redux'
 
 import TheTitleBar from "./components/TheTitleBar/index";
 import TheMeta from "./components/TheMeta";
@@ -11,7 +11,7 @@ import ThePlayer from "./components/ThePlayer";
 
 const IndexApp: React.FunctionComponent<{ data: ITrack[] }> = ({ data }) => {
 
-    const nowPlay: number = useSelector( (state: any) => state.nowPlay);
+    const nowPlay: number = useSelector((state: any) => state.nowPlay);
 
     return <Layout>
         <TheTitleBar />
