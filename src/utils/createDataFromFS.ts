be1c52c097c1d72455e04b106c95188b702580d5
Saveliy <seva007@hotmail.com>
2021-05-17 15:47:41 +0300

    Correct returned skin_id

--- src/utils/createDataFromFS.ts
+++ src/utils/createDataFromFS.ts
@@ -1,26 +1,19 @@
 import OS from "os";
-
 import { parseFile } from "music-metadata";
 import { IAudioMetadata } from "music-metadata/lib/type";
-
 import getPathsFS from "./getPathsFS";
-
 import createBase64String from "./createBase64String";
 import createMp3ObjectURL from "./createMp3ObjectURL";
 
 function createDataFromFS(skinsLength?: any) {
-
     const PATHS_DIRS = new Array(
         `${OS.userInfo().homedir}/Music/test`,
     );
 
-    const data = getPathsFS(PATHS_DIRS)
+    const data = getPathsFS(PATHS_DIRS).map(async(URI: string) => {
 
-    .map(async(URI: string) => {
-
-        if( URI.split(".")[1] === "mp3" ) {
+        if(URI.split(".")[1] === "mp3") {
             const metadata: IAudioMetadata = await parseFile(URI);
-
             return {
                 url:        createMp3ObjectURL(URI),
                 artist:     metadata.common.artist,
@@ -29,11 +22,10 @@ function createDataFromFS(skinsLength?: any) {
                 duration:   metadata.format.duration,
                 title:      metadata.common.title,
                 year:       metadata.common.year,
-                skin_id:    Math.floor(Math.random() * skinsLength)
+                skin_id:    skinsLength !== undefined ? Math.floor(Math.random() * skinsLength) : undefined
             }; 
         };
     });
-
     return Promise.all(data);
 };
 
