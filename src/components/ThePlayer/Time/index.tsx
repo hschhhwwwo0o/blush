a24485ec087110b606f57fcdfd3663d8cb027401
Saveliy <seva007@hotmail.com>
2021-05-17 08:25:35 +0300

    Correct Time component

--- src/components/ThePlayer/Time/index.tsx
+++ src/components/ThePlayer/Time/index.tsx
@@ -2,7 +2,6 @@ import React, { useRef } from "react";
 import { useDispatch } from "react-redux";
 import { CHANGE_TIMELINE } from "../../../redux/actions";
 import formatTime from "../../../utils/formatTime";
-
 import { ITimeElement } from "./interface";
 import "./index.styl";
 
@@ -12,8 +11,12 @@ const Time: React.FunctionComponent<ITimeElement> = (props) => {
     const dispatch = useDispatch();
 
     let valueLine = (props.currentTimeLine * 100) / props.duration;
-    let currentColor = props.color;
-    let gradient = `-webkit-linear-gradient(left, ${currentColor} 0%, ${currentColor} ${valueLine}%, #fff ${valueLine}%, #fff 100%)`;
+    let gradient = `-webkit-linear-gradient(left, ${props.color} 0%, ${props.color} ${valueLine}%, #fff ${valueLine}%, #fff 100%)`;
+
+    function onChangeTimeLine(e: any) {
+        dispatch({ type: CHANGE_TIMELINE, currentTimeLine: props.audioRef.current.duration * (+e.target.value / props.duration) });
+        props.audioRef.current.currentTime = props.audioRef.current.duration * (+e.target.value / props.duration);
+    };
 
     return <>
         <div id="ThePlayer__timer" style={{ borderTop: `4px solid ${props.color}` }}>
@@ -26,10 +29,7 @@ const Time: React.FunctionComponent<ITimeElement> = (props) => {
                     max             = { props.duration } 
                     step            = "any"
                     value           = { props.currentTimeLine }
-                    onChange        = { e => {
-                        dispatch({ type: CHANGE_TIMELINE, currentTimeLine: props.audioRef.current.duration * (+e.target.value / props.duration) });
-                        props.audioRef.current.currentTime = props.audioRef.current.duration * (+e.target.value / props.duration)
-                    }}
+                    onChange        = {(e) => { onChangeTimeLine(e) }}
                     style={{ background: gradient }}
                 />
                 <div id="ThePlayer__time">
