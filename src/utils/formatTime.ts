d86ba0906e7eae1ad034da935d1af2116da8af94
Saveliy <seva007@hotmail.com>
2021-05-17 15:51:03 +0300

    CS

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -6,16 +6,16 @@ function formatTime(time: number): string {
     let formatSeconds;
 
     if( secs.toFixed().toString().length === 1 ) {
-        formatSeconds = `0${secs.toFixed().toString()}`
+        formatSeconds = `0${secs.toFixed().toString()}`;
     } else {
-        formatSeconds = secs.toFixed().toString()
+        formatSeconds = secs.toFixed().toString();
     }
     
     let formatMinutes = minutes.toString().split(".")[0];
 
-    if( formatSeconds == "60" ) {
+    if(formatSeconds == "60") {
         minutes++;
-        return `${minutes.toString().split(".")[0]}:00`
+        return `${minutes.toString().split(".")[0]}:00`;
     }
 
     return `${formatMinutes}:${formatSeconds}`;
