ef66e9c9082c80c381c164f942ddb93f14248fdc
Saveliy <seva007@hotmail.com>
2021-05-20 18:50:51 +0300

    RR

--- src/utils/getSkins.js
+++ src/utils/getSkins.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 
 export default async function getSkins() {
-    const res = await fetch("https://api.jsonbin.io/b/60a0b5841ad3151d4b30d6a9/5");
+    const res = await fetch("https://api.jsonbin.io/b/60a0b5841ad3151d4b30d6a9/8");
     const skins = await res.json();
 
     return skins;
