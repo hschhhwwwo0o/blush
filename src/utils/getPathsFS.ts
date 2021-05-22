28870325e91cb5479b4cb4181fff74d5d6755f25
Saveliy <seva007@hotmail.com>
2021-05-22 08:32:14 +0300

    Correct getPathsFS function

--- src/utils/getPathsFS.ts
+++ src/utils/getPathsFS.ts
@@ -3,15 +3,15 @@ import { readdirSync } from "fs";
 function getPathsFS(PATHS_DIRS: string[]): string[] {
     let fls = new Array();
 
-    PATHS_DIRS.forEach( (path: string) => {
+    PATHS_DIRS.forEach((path: string) => {
+        if(path !== "") {
+            const files = readdirSync(path);
+            const paths = files.map( (file) => {
+                return `${path}\\${file}`
+            });
 
-        const files = readdirSync(path);
-
-        const paths = files.map( (file) => {
-            return `${path}\\${file}`
-        });
-
-        fls = [...fls, paths];
+            fls = [...fls, paths];
+        };
     });
 
     return fls[0];
