723622b43df436454ef78a6801dee15934f1e337
Saveliy <seva007@hotmail.com>
2021-05-24 19:35:54 +0300

    Remove random

--- src/utils/createDataFromFS.ts
+++ src/utils/createDataFromFS.ts
@@ -5,7 +5,6 @@ import { IAudioMetadata } from "music-metadata/lib/type";
 import InitializeDirectories from "./InitializeDirectories";
 import getPathsFS from "./getPathsFS";
 import checkFileExtension from "./checkFileExtension";
-import createBase64String from "./createBase64String";
 import createMp3ObjectURL from "./createMp3ObjectURL";
 
 function createDataFromFS(skinsLength?: number) {
@@ -14,20 +13,20 @@ function createDataFromFS(skinsLength?: number) {
         `${OS.userInfo().homedir}/Music/testy`,
     ]);
 
+    let skin = Math.floor(Math.random() * skinsLength);
+
     const data = getPathsFS(dirs).map(async(URI: string) => {
         let ext = path.extname(URI);
         if(checkFileExtension(ext)) {
             const metadata: IAudioMetadata = await parseFile(URI);
+            skin < skinsLength - 2 ? skin++ : skin = 0;
             return {
                 url:        createMp3ObjectURL(URI),
                 artist:     metadata.common?.artist,
-                album:      metadata.common?.album,
-                cover:      metadata.common.picture !== undefined ? createBase64String(metadata.common.picture[0]) : undefined,
                 duration:   metadata.format?.duration,
                 title:      metadata.common?.title,
-                year:       metadata.common?.year,
-                skin_id:    skinsLength !== undefined ? Math.floor(Math.random() * skinsLength) : undefined
-            }; 
+                skin_id:    skin
+            };
         };
     });
     return Promise.all(data);
