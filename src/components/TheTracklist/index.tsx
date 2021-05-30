3663d1b1840e4347bbafdc647a31ec767196577c
Saveliy <seva007@hotmail.com>
2021-05-30 08:51:13 +0300

    Correct TheTracklist styles

--- src/components/TheTracklist/index.tsx
+++ src/components/TheTracklist/index.tsx
@@ -5,7 +5,30 @@ import "./index.styl";
 const TheTracklist: React.FunctionComponent<ITheTracklist> = ({ data }) => {
     return <>
         <div id="theTracklist">
-            
+            <h1>
+                Entropy
+            </h1>
+            <h1>
+                Frail State Of Mind
+            </h1>
+            <h1>
+                Out Of The Black
+            </h1>
+            <h1>
+                Mind
+            </h1>
+            <h1>
+                Hello Caves
+            </h1>
+            <h1>
+                The Moon I Loved
+            </h1>
+            <h1>
+                Butterfly Effect
+            </h1>
+            <h1>
+                Me
+            </h1>
         </div>
     </>
 }
