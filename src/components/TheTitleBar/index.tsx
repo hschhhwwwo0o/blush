8d9444565d07d0686b57b0a9f9c3a89b2d831935
Saveliy <seva007@hotmail.com>
2021-05-30 09:06:30 +0300

    Render player if isTracklist

--- src/components/TheTitleBar/index.tsx
+++ src/components/TheTitleBar/index.tsx
@@ -1,20 +1,35 @@
 import React from "react";
 import { remote } from "electron";
+import { useDispatch, useSelector } from "react-redux";
+import { IStore } from "../../redux/interface.store";
 import ListICO from "./ListICO";
 import "./index.styl";
+import { SET_TRACKLIST } from "../../redux/actions";
 
 const TheTitleBar: React.FunctionComponent = () => {
+    const dispath = useDispatch();
+    const { isTracklist } = useSelector((state: IStore) => {
+        return {
+            isTracklist: state.isTracklist,
+        };
+    });
     const __windowClose = () => {
         window.top.close();
-    }
+    };
     const __windowMinimize = () => {
         remote.getCurrentWindow().minimize();
-    }
+    };
+    const windowOpenTracklist = () => {
+        dispath({
+            type: SET_TRACKLIST,
+            isTracklist: !isTracklist,
+        });
+    };
     return <>
         <header>
             <section id="TheTitlebar">
                 <div id="TheTitlebar__logo">BLUSH</div>
-                <div>
+                <div onClick={windowOpenTracklist}>
                     <ListICO />
                 </div>
                 <div id="TheTitlebar__controls">
