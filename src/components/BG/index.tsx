d4ee3710d881bda9b69f1a8268fe7483c68aa667
Saveliy <seva007@hotmail.com>
2021-05-30 10:36:02 +0300

    Bg bright isTracklist

--- src/components/BG/index.tsx
+++ src/components/BG/index.tsx
@@ -1,12 +1,24 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { IBG } from "./interface";
+import { IStore } from "../../redux/interface.store";
 import "./index.styl";
 
 const BG: React.FunctionComponent<IBG> = ({ online, image }) => {
     const useImage = { backgroundImage: `url(${image})` };
     const useStandartColor = { backgroundColor: "#461027" };
 
-    return <div id="bg" style={ online ? useImage : useStandartColor } />
+    const { isTracklist } = useSelector((store: IStore) => {
+        return {
+            isTracklist: store.isTracklist,
+        }
+    })
+
+    return <div 
+        id="bg" 
+        style={ online ? useImage : useStandartColor } 
+        className={ isTracklist ? "bright" : "" }
+    />
 }
 
 export default BG;
