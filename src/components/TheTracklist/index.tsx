7261f446f5361ac221e25bd5a67abb89f8302fe7
Saveliy <seva007@hotmail.com>
2021-05-30 08:08:50 +0300

    Correct TheTracklist component

--- src/components/TheTracklist/index.tsx
+++ src/components/TheTracklist/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { ITheTracklist } from "./interface";
 import "./index.styl";
 
-const TheTracklist: React.FunctionComponent = () => {
+const TheTracklist: React.FunctionComponent<ITheTracklist> = ({ data }) => {
     return <>
         <div id="theTracklist">
             
---
+++ src/components/TheTracklist/interface.ts
@@ -0,0 +1,5 @@
+import { ITrack } from "../../types";
+
+export interface ITheTracklist {
+    data?: ITrack[]
+}
