d42c004e734afc2d8d433370ed22c6386ef2ff77
Saveliy <seva007@hotmail.com>
2021-05-31 08:42:12 +0300

    Correct NotFound

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { INotFound } from "./interface";
 import "./index.styl";
-import Layout from "../../layout";
 import TheTitlebar from "../TheTitleBar";
 import TheMeta from "../TheMeta";
 
 const NotFound: React.FunctionComponent<INotFound> = ({ image, color }) => {
-    return <Layout>
+    return <>
         <TheTitlebar />
         <TheMeta title="Music Not Found" artist="Add your music on E:/Music directory" />
-    </Layout>
+    </>
 } 
 
 export default NotFound;
