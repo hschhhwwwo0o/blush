0608315cd832ecd7d138158cc41cc77996baf9a0
Saveliy <seva007@hotmail.com>
2021-05-31 08:39:34 +0300

    Initial NotFound page

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { INotFound } from "./interface";
 import "./index.styl";
+import Layout from "../../layout";
+import TheTitlebar from "../TheTitleBar";
+import TheMeta from "../TheMeta";
 
 const NotFound: React.FunctionComponent<INotFound> = ({ image, color }) => {
-    return <>
-        Music Not Found
-    </>
+    return <Layout>
+        <TheTitlebar />
+        <TheMeta title="Music Not Found" artist="Add your music on E:/Music directory" />
+    </Layout>
 } 
 
 export default NotFound;
