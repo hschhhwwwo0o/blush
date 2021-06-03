18484487213b661769175dc84871801ea7e3c50a
Saveliy <seva007@hotmail.com>
2021-06-03 11:06:01 +0300

    Docs

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -4,6 +4,16 @@ interface ILayout {
     children: ReactChild | ReactChildren | JSX.Element [];
 }
 
+/**
+ * 
+ * Layout component
+ * 
+ * 
+ * @param children JSX component || components
+ * 
+ * @returns {JSX} Return Layout component
+*/
+
 const Layout: React.FunctionComponent<ILayout> = ({ children }) => {
     return <>
         <main id="app">
