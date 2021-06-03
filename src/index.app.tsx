eaa1605f46599e1ec07d9c7f541b1967b11bdbdd
Saveliy <seva007@hotmail.com>
2021-06-03 13:39:50 +0300

    Doc render

--- src/index.app.tsx
+++ src/index.app.tsx
@@ -9,22 +9,65 @@ import TheMeta from "./components/TheMeta";
 import ThePlayer from "./components/ThePlayer";
 import BG from "./components/BG";
 
+/**
+ * 
+ * Interface for app
+ * 
+ * @interface 
+ * 
+*/
+
 interface IApp {
     data: ITrack[]
     skins?: ISkin[] | undefined
     online?: boolean
 }
 
+/**
+ * 
+ * JSX App component
+ * 
+ * @component
+ * 
+ * @param {Array} data ITrack[]. Array music
+ * @param {Array} skins ISkin[]. Array skins
+ * @param {boolean} online Online/Offline status
+ * 
+ * @returns {JSX}
+ */
+
 const App: React.FunctionComponent<IApp> = ({ data, skins, online }) => {
+
+    /**
+     * 
+     * React/Redux function for get global redux state
+     * Get nowPlay: number field for music play
+     * 
+    */
     const { nowPlay }: IStoreSample = useSelector((state: IStore) => {
         return {
             nowPlay: state.nowPlay
         }
     });
 
+    /**
+     * 
+     * Returns the skin for the current track 
+     * 
+     * @param {number} nowPlay Current track 
+     * 
+    */
     function getSkin(nowPlay: number): ISkin {
         return skins[data[nowPlay].skin_id];
     };
+
+    /**
+     * 
+     * Returns the skin for the current track 
+     * 
+     * @param {number} nowPlay Current track 
+     * 
+    */
     function getTrack(nowPlay: number): ITrack {
         return data[nowPlay];
     };
